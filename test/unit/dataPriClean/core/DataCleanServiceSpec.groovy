070ff19d4190e9ad14ab65ca45c8c549ad005f06
Dejun Huang <djhuang21@gmail.com>
2015-07-13 10:16:19 -0400

    Add performance test

--- test/unit/dataPriClean/core/DataCleanServiceSpec.groovy
+++ test/unit/dataPriClean/core/DataCleanServiceSpec.groovy
@@ -1,5 +1,7 @@
 package dataPriClean.core
 
+import java.util.List;
+
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
@@ -15,6 +17,245 @@ class DataCleanServiceSpec extends Specification {
     def cleanup() {
     }
 
-    void "test something"() {
+	//small dataset load test with 6 records
+    void "test loadTargetDataset"() {
+		def before = System.currentTimeMillis()
+		
+		given:
+		String url = "/Users/thomas/Documents/Programming/DataPrivacy/resource/data/testdata1/address.csv"
+		String fileName = "address.csv"
+		String fdUrl = "/Users/thomas/Documents/Programming/DataPrivacy/resource/data/testdata1/address_fd2.csv"
+		char separator = ','
+		char quoteChar = '\''
+		
+		when:
+		def dataset = service.loadTargetDataset(url, fileName, fdUrl, separator, quoteChar)
+		
+		def after = System.currentTimeMillis()
+		
+		then:
+		dataset != null
+		
+		println("Number of records: " + dataset.getRecords.size())
+		println("Running time: " + (after - before) + "ms")
     }
+	
+	//dataset load test with 3,000 records
+	void "test loadTargetDataset1"() {
+		def before = System.currentTimeMillis()
+		
+		given:
+		String url = "/Users/thomas/Documents/Programming/DataPrivacy/resource/data/testdata4/Japan_trials.csv"
+		String fileName = "Japan_trials.csv"
+		String fdUrl = "/Users/thomas/Documents/Programming/DataPrivacy/resource/data/testdata4/fds.csv";
+		char separator = ','
+		char quoteChar = '"'
+		
+		when:
+		def dataset = service.loadTargetDataset(url, fileName, fdUrl, separator, quoteChar)
+		
+		def after = System.currentTimeMillis()
+		
+		then:
+		dataset != null
+		
+		println("Number of records: " + dataset.getRecords.size())
+		println("Running time: " + (after - before) + "ms")
+	}
+	
+	//dataset load test with 10,000 records
+	void "test loadTargetDataset2"() {
+		def before = System.currentTimeMillis()
+		
+		given:
+		String url = "/Users/thomas/Documents/Programming/DataPrivacy/resource/data/testdata4/Canada_trials.csv"
+		String fileName = "Canada_trials.csv"
+		String fdUrl = "/Users/thomas/Documents/Programming/DataPrivacy/resource/data/testdata4/fds.csv";
+		char separator = ','
+		char quoteChar = '"'
+		
+		when:
+		def dataset = service.loadTargetDataset(url, fileName, fdUrl, separator, quoteChar)
+		
+		def after = System.currentTimeMillis()
+		
+		then:
+		dataset != null
+		
+		println("Number of records: " + dataset.getRecords.size())
+		println("Running time: " + (after - before) + "ms")
+	}
+	
+	//test small dataset and constraint
+	void "test findViolations"() {
+		def before = System.currentTimeMillis()
+		
+		given:
+		String url = "/Users/thomas/Documents/Programming/DataPrivacy/resource/data/testdata1/address.csv"
+		String fileName = "address.csv"
+		String fdUrl = "/Users/thomas/Documents/Programming/DataPrivacy/resource/data/testdata1/address_fd2.csv"
+		char separator = ','
+		char quoteChar = '\''
+		
+		when:
+		def dataset = service.loadTargetDataset(url, fileName, fdUrl, separator, quoteChar)
+		def vio = service.findViolations(dataset)
+		
+		def after = System.currentTimeMillis()
+		
+		then:
+		dataset != null
+		vio != null
+		
+		println ("Number of violations: " + calcVioNum(vio))
+		//		println ("violations:" + vio)
+		println("Number of records: " + dataset.getRecords.size())
+		println("Running time: " + (after - before) + "ms")
+	}
+	
+	//test small dataset and constraint
+	void "test findViolations with single fd"() {
+		def before = System.currentTimeMillis()
+		
+		given:
+		String url = "/Users/thomas/Documents/Programming/DataPrivacy/resource/data/testdata1/address.csv"
+		String fileName = "address.csv"
+		String fdUrl = "/Users/thomas/Documents/Programming/DataPrivacy/resource/data/testdata1/address_fd.csv"
+		char separator = ','
+		char quoteChar = '\''
+		
+		when:
+		def dataset = service.loadTargetDataset(url, fileName, fdUrl, separator, quoteChar)
+		def vio = service.findViolations(dataset)
+		
+		def after = System.currentTimeMillis()
+		
+		then:
+		dataset != null
+		vio != null
+		
+		println ("number of violations: " + calcVioNum(vio))
+		//		println ("violations:" + vio)
+		println("Number of records: " + dataset.getRecords.size())
+		println("Running time: " + (after - before) + "ms")
+	}
+	
+	//test large dataset and constraint
+	void "test findViolations1"() {
+		def before = System.currentTimeMillis()
+		
+		given:
+		String url = "/Users/thomas/Documents/Programming/DataPrivacy/resource/data/testdata4/Japan_trials.csv"
+		String fileName = "Japan_trials.csv"
+		String fdUrl = "/Users/thomas/Documents/Programming/DataPrivacy/resource/data/testdata4/fds.csv";
+		char separator = ','
+		char quoteChar = '"'
+		
+		when:
+		def dataset = service.loadTargetDataset(url, fileName, fdUrl, separator, quoteChar)
+		def vio = service.findViolations(dataset)
+		
+		def after = System.currentTimeMillis()
+		
+		then:
+		dataset != null
+		vio != null
+		
+		println ("number of violations: " + calcVioNum(vio))
+		//		println ("violations1:" + vio)
+		println("Number of records: " + dataset.getRecords.size())
+		println("Running time: " + (after - before) + "ms")
+	}
+	
+	//test large dataset and constraint
+	void "test findViolations1 with single fd"() {
+		def before = System.currentTimeMillis()
+		
+		given:
+		String url = "/Users/thomas/Documents/Programming/DataPrivacy/resource/data/testdata4/Japan_trials.csv"
+		String fileName = "Japan_trials.csv"
+		String fdUrl = "/Users/thomas/Documents/Programming/DataPrivacy/resource/data/testdata4/fds1.csv";
+		char separator = ','
+		char quoteChar = '"'
+		
+		when:
+		def dataset = service.loadTargetDataset(url, fileName, fdUrl, separator, quoteChar)
+		def vio = service.findViolations(dataset)
+		
+		def after = System.currentTimeMillis()
+		
+		then:
+		dataset != null
+		vio != null
+		
+		println ("number of violations: " + calcVioNum(vio))
+		//		println ("violations1:" + vio)
+		println("Number of records: " + dataset.getRecords.size())
+		println("Running time: " + (after - before) + "ms")
+	}
+	
+	//test large dataset and constraint
+	void "test findViolations2"() {
+		def before = System.currentTimeMillis()
+		
+		given:
+		String url = "/Users/thomas/Documents/Programming/DataPrivacy/resource/data/testdata4/Canada_trials.csv"
+		String fileName = "Canada_trials.csv"
+		String fdUrl = "/Users/thomas/Documents/Programming/DataPrivacy/resource/data/testdata4/fds.csv";
+		char separator = ','
+		char quoteChar = '"'
+		
+		when:
+		def dataset = service.loadTargetDataset(url, fileName, fdUrl, separator, quoteChar)
+		def vio = service.findViolations(dataset)
+		
+		def after = System.currentTimeMillis()
+		
+		then:
+		dataset != null
+		vio != null
+		
+		println ("number of violations: " + calcVioNum(vio))
+		//		println ("violations2:" + vio)
+		println("Number of records: " + dataset.getRecords.size())
+		println("Running time: " + (after - before) + "ms")
+	}
+	
+	//test large dataset and constraint
+	void "test findViolations2 with single fd"() {
+		def before = System.currentTimeMillis()
+		
+		given:
+		String url = "/Users/thomas/Documents/Programming/DataPrivacy/resource/data/testdata4/Canada_trials.csv"
+		String fileName = "Canada_trials.csv"
+		String fdUrl = "/Users/thomas/Documents/Programming/DataPrivacy/resource/data/testdata4/fds.csv";
+		char separator = ','
+		char quoteChar = '"'
+		
+		when:
+		def dataset = service.loadTargetDataset(url, fileName, fdUrl, separator, quoteChar)
+		def vio = service.findViolations(dataset)
+		
+		def after = System.currentTimeMillis()
+		
+		then:
+		dataset != null
+		vio != null
+		
+		println ("number of violations: " + calcVioNum(vio))
+		//		println ("violations2:" + vio)
+		println("Number of records: " + dataset.getRecords.size())
+		println("Running time: " + (after - before) + "ms")
+	}
+	
+	//calculate the total number of violations
+	int calcVioNum (vios) {
+		int count = 0
+		for (vio in vios) {
+			count += vio.violMap.size()
+		}
+		return count
+	}
 }
+
+
