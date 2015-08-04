ec4e5d1cc657455dbc287e493dc9ea947f2ff00c
Dejun Huang <djhuang21@gmail.com>
2015-08-04 14:32:05 -0400

    Add default target & master agent

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -17,15 +17,34 @@ class BootStrap {
 		Date date = new Date()
 		
 		def agent = new Agent(
-			name: "thomas",
-			username: "thomas",
-			password: "thomas",
+			name: "target",
+			username: "target",
+			password: "target",
 			dateCreated: date,
 			dateUpdated: date,
 			role: "Target"
 			)
 		
 		if (agent.save()) {
+			println "new agent created sucessfully!"
+		}
+		else {
+			println "new agent created failed!"
+			agent.errors.each{
+				println it
+			}
+		}
+		
+		def agentMaster = new Agent(
+			name: "master",
+			username: "master",
+			password: "master",
+			dateCreated: date,
+			dateUpdated: date,
+			role: "Master"
+			)
+		
+		if (agentMaster.save()) {
 			println "new agent created sucessfully!"
 		}
 		else {
